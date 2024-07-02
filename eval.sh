71cc35d3df7f10f8339f1000701db02204b81d38
ErlebnisW <787368748w@gmail.com>
2024-07-02 17:18:52 +0800

    add output dir

--- eval.sh
+++ eval.sh
@@ -3,3 +3,4 @@ export OPENAI_CLIENT_CONFIG_PATH="/data1/WM_workspace/alpaca_eval/client_configs
 
 alpaca_eval --model_outputs /data1/WM_workspace/alpaca_eval/output/qwen2-alpaca_sp_3/sp_3/generated_answers.json \
             --annotators_config claude_3_opus_ranking \
+            --output_path /data1/WM_workspace/alpaca_eval/output/results
