e3e70d788051b088cb2e465a2685116e715cceb9
ErlebnisW <787368748w@gmail.com>
2024-07-23 01:55:06 +0800

    set temperature as 0.7

--- vllm_generate.sh
+++ vllm_generate.sh
@@ -1,12 +1,11 @@
 export CUDA_VISIBLE_DEVICES=1
 export HF_ENDPOINT="https://hf-mirror.com"
 
-# name=gemma-2b_alpaca_sft 
-name=test
+name=lr_3e-6_fix_temp_1.0/sp_3
 
 # GPT-4 evaluation
 python3 -m alpaca_eval.vllm_generate \
-    --model_name_or_path /data1/WM_workspace/MDSPO/output/gemma-2b_alpaca_sft/ \
+    --model_name_or_path /data1/WM_workspace/MDSPO/output/lr_3e-6_fix_temp_1.0/sp_3/ \
     --output_dir /data1/WM_workspace/alpaca_eval/output/${name} \
     --temperature 0.7 \
     --max_tokens 2048 \
