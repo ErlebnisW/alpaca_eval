6db45fed3538099be3971d2ce727f72ea62513af
ErlebnisW <787368748w@gmail.com>
2024-08-05 00:26:30 +0800

    add shell scripts for vllm generate

--- vllm_generate.sh
+++ vllm_generate.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=1
 export HF_ENDPOINT="https://hf-mirror.com"
 
-name=lr_3e-6_fix_temp_1.0/sp_3
+name=gemma-2b_alpaca_sft
 
 # GPT-4 evaluation
 python3 -m alpaca_eval.vllm_generate \
-    --model_name_or_path /data1/WM_workspace/MDSPO/output/lr_3e-6_fix_temp_1.0/sp_3/ \
+    --model_name_or_path /data1/WM_workspace/MDSPO/output/fix_temp_1.0/ \
     --output_dir /data1/WM_workspace/alpaca_eval/output/${name} \
     --temperature 0.7 \
     --max_tokens 2048 \
