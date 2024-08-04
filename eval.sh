b590556d53463a94f7d5e413750792334043167d
ErlebnisW <787368748w@gmail.com>
2024-08-05 00:25:33 +0800

    add reference_outputs

--- eval.sh
+++ eval.sh
@@ -1,6 +1,8 @@
 export HF_ENDPOINT="https://hf-mirror.com"
-export OPENAI_CLIENT_CONFIG_PATH="/data1/WM_workspace/alpaca_eval/client_configs/custom.yaml"
+export OPENAI_CLIENT_CONFIG_PATH="/data1/WM_workspace/alpaca_eval/client_configs/deepseek.yaml"
 
-alpaca_eval --model_outputs /data1/WM_workspace/alpaca_eval/output/qwen2-alpaca_sp_3/sp_3/generated_answers.json \
-            --annotators_config claude_3_opus_ranking \
-            --output_path /data1/WM_workspace/alpaca_eval/output/results
+name=gemma_2b_alpaca_sft
+alpaca_eval --model_outputs /data1/WM_workspace/alpaca_eval/output/fix_temp_1/generated_answers.json \
+            --annotators_config deepseek-chat \
+            --output_path /data1/WM_workspace/alpaca_eval/output/results/${name} \
+            # --reference_outputs /data1/WM_workspace/alpaca_eval/output/gemma-2b_alpaca_sft/generated_answers.json
